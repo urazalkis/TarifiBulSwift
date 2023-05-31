20e10365ba200a85b60158df7d702620b9cccc0d
urazalkis <43914441+urazalkis@users.noreply.github.com>
2023-05-31 16:15:59 +0300

    Initial Commit

---
+++ TarifiBul/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TarifiBul
+//
+//  Created by Uraz Alkış on 31.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
